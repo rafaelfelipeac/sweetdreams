9a7b04df82bb087f42a1265144db2b371edc7eae
Rafael <rafael.felipe.sccp@gmail.com>
2016-10-21 16:04:42 -0200

    initial files

--- app/src/main/java/com/sd/rafael/sweetdreams/MainActivity.java
+++ app/src/main/java/com/sd/rafael/sweetdreams/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
