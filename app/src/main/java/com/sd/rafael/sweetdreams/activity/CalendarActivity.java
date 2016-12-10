8d03ff71b09dc9bbddbb2c03618a838b16f4f26b
Rafael <rafael.felipe.sccp@gmail.com>
2016-12-09 23:26:46 -0200

    changed CalendarView for CompactCalendarView

--- app/src/main/java/com/sd/rafael/sweetdreams/activity/CalendarActivity.java
+++ app/src/main/java/com/sd/rafael/sweetdreams/activity/CalendarActivity.java
@@ -1,26 +1,68 @@
 package com.sd.rafael.sweetdreams.activity;
 
+import android.graphics.Color;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+import com.sd.rafael.sweetdreams.DAO.DreamDAO;
 import com.sd.rafael.sweetdreams.R;
+import com.sd.rafael.sweetdreams.models.Dream;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    CalendarView calendar;
+    CompactCalendarView calendar;
+    TextView textView;
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        calendar = (CalendarView) findViewById(R.id.calendarView);
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        DreamDAO dao = new DreamDAO(this);
+        final List<Dream> dreams = dao.Read();
+
+        textView = (TextView) findViewById(R.id.textViewCalendar);
+
+        calendar = (CompactCalendarView) findViewById(R.id.compactCalendarView);
+        calendar.setShouldShowMondayAsFirstDay(false);
+
+        for(Dream dream : dreams) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = null;
+            try {
+                date = sdf.parse(dream.getDay() + "/" + dream.getMonth() + "/" + dream.getYear());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Event event = new Event(Color.GREEN, date.getTime());
+            calendar.addEvent(event);
+        }
+
+        calendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Snackbar.make(calendar, dayOfMonth + "/" + month + "/" + year, Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+            public void onDayClick(Date dateClicked) {
+                textView.setText(dateFormatForMonth.format(dateClicked));
+                Snackbar.make(calendar, dateClicked.toString(), Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                //toolbar.setTitle(dateFormatForMonth.format(firstDayOfNewMonth));
+                textView.setText(dateFormatForMonth.format(firstDayOfNewMonth));
+
             }
         });
     }
